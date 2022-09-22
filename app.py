281f386c4b6b1740a6bba5d47e225c836d31d7f5
suneelyadava1 <suneelyadava@microsoft.com>
2022-09-22 14:42:12 +0530

    test

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
-number = st.number_input('Insert a number')
-st.write('The current number is ', number)
+
+st.title("Hello Let's find whether an URL is DGA or Not!")
+
+if st.button('Check DGA'):
+    dga_url=st.text_input("Enter the URL")
+    st.title(dga_url)
+    clean = 0.262
+    DGA = 0.92
+    st.number_input('clean', clean)
+    st.number_input('DGA', DGA)
